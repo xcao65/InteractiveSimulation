c7b2ad77248bbac4236e9f185bc533732fcf47b3
Natalia Gilbertson <nataliag@unity3d.com>
2022-11-14 16:32:23 -0800

    add NPC

---
+++ Assets/Tutorial/Scripts/NPC.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// navmesh moved to UnityEngine.AI in 2017
+#if !UNITY_5
+using UnityEngine.AI;
+#endif
+
+
+public class NPC : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveTo( Vector3 pos )
+    {
+        agent.destination = pos;
+    }
+}
