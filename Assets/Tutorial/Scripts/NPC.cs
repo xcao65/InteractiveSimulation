f82087edb8cdaad7e846925af08d0f3579caab60
Natalia Gilbertson <nataliag@unity3d.com>
2022-12-02 13:18:36 -0800

    unity kicking off process works

--- Assets/Tutorial/Scripts/NPC.cs
+++ Assets/Tutorial/Scripts/NPC.cs
@@ -5,18 +5,43 @@
 using UnityEngine.AI;
 #endif
 
+using System.Diagnostics;
+
 
 public class NPC : MonoBehaviour
 {
     NavMeshAgent agent;
+    ProcessManager pm;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        var psi = new ProcessStartInfo();
+        psi.FileName = $@"{Application.dataPath}/Scripts/NPCBrain/.venv/bin/python3.7";
+
+        UnityEngine.Debug.Log($"File path to python executable: {psi.FileName}");
+
+        var script = $@"{Application.dataPath}/Scripts/NPCBrain/npc_brain.py";
+
+        psi.Arguments = $"{script}"; // need quotes?
+        psi.UseShellExecute = false;
+        psi.CreateNoWindow = true;
+        psi.RedirectStandardOutput = true;
+
+        pm = new ProcessManager(psi);
+
+        pm.Start();
     }
 
     public void MoveTo( Vector3 pos )
     {
         agent.destination = pos;
+    }
+
+    void OnApplicationQuit()
+    {
+        //ScenarioPlayer.Instance.StopListening();
+        pm.Stop();
     }
 }
---
+++ Assets/Tutorial/Scripts/ProcessManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+using System.Net.WebSockets;
+using System.Net.Http;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+public class ProcessManager
+{
+    Task m_ListenThread;
+    bool m_IsListening;
+    CancellationTokenSource m_CancellationTokenSource;
+    ProcessStartInfo psi;
+    Process proc;
+    public ProcessManager(ProcessStartInfo psi)
+    {
+        m_CancellationTokenSource = new CancellationTokenSource();
+
+        this.psi = psi;
+    }
+
+    public void Start()
+    {
+        if (m_IsListening) return;
+
+        m_ListenThread = Task.Run(() => ExecuteProcess(m_CancellationTokenSource.Token));
+
+        m_IsListening = true;
+
+        UnityEngine.Debug.Log("Started process thread.");
+    }
+
+    async Task ExecuteProcess(CancellationToken cancellationToken)
+    {
+        UnityEngine.Debug.Log("Executing process...");
+        
+        proc = Process.Start(psi);
+    }
+
+    public void Stop(int timeout = 2000)
+    {
+        if (!m_IsListening) return;
+
+        if (m_ListenThread == null) return; // Not started yet
+
+        if (proc != null)
+            proc.Kill();
+
+        m_CancellationTokenSource.Cancel();
+
+        // Wait for queued requests to go out
+        try
+        {
+            m_ListenThread.Wait(timeout);
+        }
+        catch (AggregateException) { } // Throws when threads are cancelled
+
+        m_IsListening = false;
+
+        UnityEngine.Debug.Log("Stopped process.");
+    }
+}
